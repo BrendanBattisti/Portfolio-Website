df86bcde60f75a766cfae884cdaf8f62705a524c
Brendan Battisti <brendan.battisti@gmail.com>
2023-06-26 22:31:21 -0400

    Removing old contents

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,8 +1,19 @@
 
 import Page from "../Page/Page"
-
+import linkedIn from "../../img/Links/LI-In-Bug.png"
+import github from "../../img/Links/github-mark-white.png"
 export default function Contact() {
-    const content = <div>Contact</div>
+    const content = <div class="social_links">
+    Check me out!
+    <a href="https://github.com/BrendanBattisti" target="_blank">
+        <img src={github} />
+    </a>
+
+    <a href="https://www.linkedin.com/in/brendan-battisti-0a548785/" target="_blank">
+        <img src={linkedIn} />
+    </a>
+
+</div>
 
     return <Page content={content} name="Contact" />
 }
